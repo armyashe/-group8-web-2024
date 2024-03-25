32cf5e9858a5b4678a59c39d69d247386d888f14
trungkien <145746252+allpedog@users.noreply.github.com>
2024-03-25 13:54:56 +0700

    1

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <title>CỬA HÀNG NỘI THẤT</title>
+    <link rel="stylesheet" href="css/style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     
-        
-    <title>Nội thất</title>
 </head>
-<body>
-    <div class="background">
+<body><div class="background">
         <div id="img">
             <div class="word" style="--i:0">
                 <h1>TẠO XU HƯỚNG CAO CẤP</h1>
@@ -36,36 +35,35 @@
             <div class="menu"> <!--thanh menu -->
                 <a href="#img">TRANG CHỦ</a>
                 <a href="#about">VỀ CHÚNG TÔI</a>
-                <a href="sanpham.html">SẢN PHẨM</a>
+                <a href="view/home.php">SẢN PHẨM</a>
                 <a href="#feedback">PHẢN HỒI</a>
-                <a href="login.html">ĐĂNG NHẬP</a>
+                <a href="view/login.php">ĐĂNG NHẬP</a>
             </div>
             <div class="right">
                 <a href="" class="toggle-menu"><i class='bx bx-menu bx-rotate-90' style="color: white;"></i></a>
             </div>
         </div>
-        
     </div>     
-    <div class="reveal products">
+    <div class="products">
         <div class="item-left">
-            <img src="./IMG/Pula-BTD11.jpg" alt="">
+            <img src="IMG/Pula-BTD11.jpg" alt="">
             <p style="margin-top: 20px; font-size: 16px;font-family: sans-serif;">Riêng cho bạn!</p>
             <p style="margin-top: 25px; font-family: SVN-Gallery Modern, Sans-serif; font-size: 25px;">Bàn trang điểm thiết kế riêng cho bạn!</p>
             <p style="margin-top: 20px;font-family:sans-serif; font-size: 16px; line-height: 30px;">Nàng có một căn phòng đẹp & chỉ còn thiếu một chiếc bàn phù hợp. Bạn muốn kích thước và màu sắc phải hoản hảo như ý bạn. Và bạn tìm thấy Lipstick.Box, không có gì tuyệt vời hơn được phục vụ nàng.</p>
         </div>
         <div class="item-right">
             <div class="right-left">
-                <img src="./IMG/sp2.png" alt="">
+                <img src="IMG/sp2.png" alt="">
                 <p style="margin-top: 25px; font-size: 16px;font-family: sans-serif;">Nội thất cho phái đẹp</p>
                 <p style="margin-top: 25px; font-family: SVN-Gallery Modern, Sans-serif; font-size: 25px;">Tìm thấy bàn & ghế trang điểm...</p>
                 <p style="margin-top: 20px;font-family:sans-serif; font-size: 16px; line-height: 30px;">…Tủ & kệ cũng ở đây, chọn online hoặc đến shop LipstickBox để team có thể tư vấn kĩ hơn.</p>
-                <img src="./IMG/sp4.png" alt="" style="height: 20%; margin-top: 20px;">
+                <img src="IMG/sp4.png" alt="" style="height: 20%; margin-top: 20px;">
             </div>
             <div class="right-right">
                 <p style="margin-top: 10px; font-size: 16px;font-family: sans-serif;">Decor & nhiều hơn nữa</p>
                 <p style="margin-top: 25px; font-family: SVN-Gallery Modern, Sans-serif; font-size: 25px;">Tìm thấy Gương Decor, Tủ kệ trang điểm,...</p>
                 <p style="margin-top: 20px;font-family:sans-serif; font-size: 12px;">…Đồ Decor và nhiều hơn nữa ở đây</p>
-                <img src="./IMG/sp3.png" alt="">
+                <img src="IMG/sp3.png" alt="">
                 <p style="margin-top: 20px; font-size: 16px;font-family: sans-serif;">Gói tư vấn – thiết kế – thi công trọn gói</p>
                 <p style="margin-top: 25px; font-family: SVN-Gallery Modern, Sans-serif; font-size: 25px;">Những thứ còn lại mà con gái cần cho căn phòng nhỏ xinh của mình...</p>
                 <p style="margin-top: 20px;font-family:sans-serif; font-size: 16px;line-height: 30px; ">Một căn phòng hoàn hảo sẽ là một căn phòng đem lại cho phái đẹp cảm xúc tích cực, phòng ngủ hay phòng tắm là một không gian nuôi dưỡng lại tâm hồn.</p>
@@ -73,7 +71,7 @@
         </div>
     </div>
     <!--phần tại sao chọn chúng tôi-->
-    <div class="reveal pledge" id="about">
+    <div class="pledge" id="about">
         <div class="text-center">
             <span>TẠI SAO CHỌN CHÚNG TÔI</span>
             <hr width="10%" color="red" size="2px" style="margin-left: 45%; margin-top: 10px;">
@@ -84,28 +82,28 @@
                         <span>Vật liệu an toàn</span>
                         <span>Nguyên liệu sạch được chọn lựa để thân thiện với môi trường và đảm bảo sức khỏe cho khách hàng</span>
                         <div class="text-img-1">
-                                <img src="./IMG/w1-b3a.png" alt="" >
+                                <img src="IMG/w1-b3a.png" alt="" >
                         </div>
                     </div>
                     <div class="info-text-2">
                         <span>Chất lượng sản phẩm</span>
                         <span>XHOME tập trung vào các sản phẩm mang tính đột phá và phù hợp với nhu cầu người sử dụng</span>
                         <div class="text-img-2">
-                                <img src="./IMG/w3-3b4.png" alt="" >
+                                <img src="IMG/w3-3b4.png" alt="" >
                         </div>
                     </div>
                     <div class="info-text-3">
                         <span>Ý tưởng sáng tạo</span>
                         <span>Luôn cập nhật và tạo ra những mẫu thiết kế nội thất độc đáo,<br> mới mẻ</span>
                         <div class="text-img-3">
-                                <img src="./IMG/w5-c4a-80-80-7d9.png" alt="" >
+                                <img src="IMG/w5-c4a-80-80-7d9.png" alt="" >
                         </div>
                     </div>
             </div>
             <div class="text-right">
                 <div class="info-text-4">
                     <div class="text-img-4">
-                            <img src="./IMG/w2-fc9.png" alt="" >
+                            <img src="IMG/w2-fc9.png" alt="" >
                     </div>
                     Đội ngũ chuyên nghiệp
                     <br>Nhân sự luôn máu lửa và làm việc đầy tâm huyết để tạo ra những sản phẩm và dịch vụ chất lượng nhất
@@ -113,7 +111,7 @@
                 </div>
                 <div class="info-text-5">
                     <div class="text-img-5">
-                            <img src="./IMG/w4-7b8-80-80-f38.png" alt="" >
+                            <img src="IMG/w4-7b8-80-80-f38.png" alt="" >
                     </div>
                     Uy tín hàng đầu
                     <br>Thương hiệu được khẳng định bằng chất lượng sản phẩm và dịch vụ trong nhiều năm qua
@@ -121,7 +119,7 @@
                 </div>
                 <div class="info-text-6">
                     <div class="text-img-6">
-                            <img src="./IMG/w6-14f.png" alt="" >
+                            <img src="IMG/w6-14f.png" alt="" >
                             
                     </div>
                     Đảm bảo sự hài lòng của khách hàng
@@ -137,20 +135,20 @@
             </div>
             <div class="feedback-right">
                 <div class="info-feedback-1">
-                    <img src="./IMG/feedback1.png" alt="">
+                    <img src="IMG/feedback1.png" alt="">
                     <p style="font-weight: bolder; font-size: 120%;">Ms Lộ Tư</p> <br>
                     <p>Sản phẩm rất đẹp. Shop hỗ trợ rất nhiệt tình, mình sẽ giới thiệu cho gia đình và bạn bè</p>
          
                     
                 </div>
                 <div class="info-feedback-2">
-                    <img src="./IMG/feedback2.png" alt="">
+                    <img src="IMG/feedback2.png" alt="">
                     <p style="font-weight: bolder; font-size: 120%;">Ms Thiên Hân</p>
                     <br>
                     <p>Cứ sợ mẹ không thích nhưng khi nhận được sản phẩm mẹ em rất vui. Giá cả phải chăng, mẫu mã hiện đại,.. Có dịp em sẽ ủng hộ tiếp!!</p>
                 </div>
                 <div class="info-feedback-3">
-                    <img src="./IMG/feedback3.png" alt="">
+                    <img src="IMG/feedback3.png" alt="">
                     <p style="font-weight: bolder; font-size: 120%;">Ms Lục Vi</p>
                     <br>
                     <p>Quá tuyệt vời. Sang trọng và đẳng cấp</p>
@@ -162,7 +160,7 @@
         <!--phần cuối cùng-->
         <div class="reveal address" >
             <div class="img-address">
-                <img src="./IMG/logo.jpg" alt="">
+                <img src="IMG/logo.jpg" alt="">
                 <div class="icon-address">
                 <i class='bx bxs-home'> 273 An Dương Vương,Phường 3,Q.5,Tp.HCM</i>
                 <i class='bx bxs-phone'> 0123456789</i>
@@ -190,8 +188,5 @@
         </div>
     </div>
     <script src="javascripts/main.js"></script>
-    <script>
-         //localStorage.clear();
-    </script>
 </body>
 </html>
